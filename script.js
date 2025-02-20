7abd922e55583318a5681010784a7d2cf7f9a96c
Soumen Dass <soumendass1357@gmail.com>
2025-02-20 11:54:33 +0530

    Updated skill section with hover animation and progress bar

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 let typed1 = new Typed('.first-line', {
-      strings: ["Welcome to My portfolio, I hope you will like it!"],
-      typeSpeed: 83,
-      loop: false
-  });
+    strings: ["Welcome to My portfolio, I hope you will like it!"],
+    typeSpeed: 83,
+    loop: false
+});
 
 
-  let typed = new Typed('.Text', {
-      strings: ["Python", "HTML", "CSS", "JavaScript"],
-      typeSpeed: 100,
-      backSpeed: 40,
-      backdelay: 2000,
-      loop: true
-  });
+let typed = new Typed('.Text', {
+    strings: ["Python", "HTML", "CSS", "JavaScript"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    backdelay: 2000,
+    loop: true
+});
 
-  let typed2 = new Typed('.Text2', {
-      strings: ["I am Soumen Dass, a dedicated first-year student pursuing a B.Tech in Computer Science and Artificial Intelligence at Newton School of technology (Rishihood University). I am passionate about technology, particularly in the areas of web development and programming. In my free time, I enjoy drawing, listening to music, and diving deeper into the latest tech trends. I am also actively learning Python and have a keen interest in Generative AI, aiming to master these skills in the near future. With a strong focus on problem-solving and continuous learning, I am on the path to becoming a proficient developer."],
-      typeSpeed: 50,
-      loop: false
-  });
+let typed2 = new Typed('.Text2', {
+    strings: ["I am Soumen Dass, a dedicated first-year student pursuing a B.Tech in Computer Science and Artificial Intelligence at Newton School of technology (Rishihood University). I am passionate about technology, particularly in the areas of web development and programming. In my free time, I enjoy drawing, listening to music, and diving deeper into the latest tech trends. I am also actively learning Python and have a keen interest in Generative AI, aiming to master these skills in the near future. With a strong focus on problem-solving and continuous learning, I am on the path to becoming a proficient developer."],
+    typeSpeed: 50,
+    loop: false
+});
+
+
+document.querySelectorAll(".card").forEach((card) => {
+    card.addEventListener("mouseenter", () => {
+        let progressBar = card.querySelector(".progress-fill");
+        let percentage = progressBar.getAttribute("data-percent");
+        progressBar.style.width = percentage + "%";
+    });
+
+    card.addEventListener("mouseleave", () => {
+        let progressBar = card.querySelector(".progress-fill");
+        progressBar.style.width = "0%"; // Reset after hover out
+    });
+});
